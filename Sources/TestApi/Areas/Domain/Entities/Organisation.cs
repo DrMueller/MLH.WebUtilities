3e8fe6c889926aed7353b716623c2576a4d02ae3
Matthias Müller <matthias.mueller@trivadis.com>
2020-10-01 18:55:52 +0200

    WIP

--- Sources/TestApi/Areas/Domain/Entities/Organisation.cs
+++ Sources/TestApi/Areas/Domain/Entities/Organisation.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace Mmu.Mlh.WebUtilities.TestApi.Areas.Domain.Entities
 {
+    [PublicAPI]
     public class Organisation : EntityBase
     {
         public ICollection<Address> Addresses { get; set; }
